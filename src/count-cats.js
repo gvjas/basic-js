75afb2e8d7e229c6ca00b2ec080a22fd1f94506e
gvjas <gvjasch@gmail.com>
2020-09-15 21:23:23 +0900

    countCats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(backyard) {
+  let number = 0;
+  for (item of backyard) {
+    number += item.filter(x => x === "^^").length;
+  }
+  return number;
+  }
+
+
