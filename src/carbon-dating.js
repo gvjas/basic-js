b205de5a2e63007e1aa2077909157f0dca91c8db
gvjas <gvjasch@gmail.com>
2020-09-16 11:20:23 +0900

    Dream team task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,9 +2,9 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  const x = parseFloat(sampleActivity)
-  if (typeof sampleActivity === 'string' && x > 0 && x < 15) {
-    const age = Math.ceil(Math.log(MODERN_ACTIVITY / x) / (0.693 / HALF_LIFE_PERIOD));
+  sampleActivity = typeof sampleActivity === 'string' ? parseFloat(sampleActivity) : false;
+  if (sampleActivity > 0 && sampleActivity < 15) {
+    const age = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
     return age;
   }
   return false;
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (typeof members !== 'object' || members === null || members[0] === undefined) {
+   return false;
+  }
+  return members
+        .filter(x => typeof x === 'string' || typeof x === 'array')
+        .map(x => x.trim())
+        .map(x => x[0].toUpperCase())
+        .sort()
+        .join('')
 };
