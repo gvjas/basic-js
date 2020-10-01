3dda75f4964029de861e99ddad28be2d342a9416
gvjas <gvjasch@gmail.com>
2020-10-01 13:49:11 +0900

    refactor: dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,11 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  if (typeof members !== 'object' || members === null || members[0] === undefined) {
+  if (!Array.isArray(members)) {
    return false;
   }
   return members
-        .filter(x => typeof x === 'string' || typeof x === 'array')
+        .filter(x => typeof x === 'string')
         .map(x => x.trim())
         .map(x => x[0].toUpperCase())
         .sort()
